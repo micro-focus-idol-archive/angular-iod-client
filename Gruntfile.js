1a70bdd0b1b0fdd5a6543a47ec6306bd36ea1346
Itsik Avidan <itsik.avidan@hp.com>
2015-06-21 17:02:42 +0300

    Adding to travis configurations to run karma separately and running grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -271,7 +271,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', [
         'clean:server',
-        'concurrent:test',
         'autoprefixer',
         'connect:test',
         'karma'
