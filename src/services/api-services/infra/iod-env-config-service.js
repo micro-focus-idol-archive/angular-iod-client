32776ae125b93c1500f2aa0a656b32eebc1cc1c8
Itsik Avidan <itsik.avidan@hp.com>
2016-02-17 16:30:32 +0200

    Changed iod-http-factory.js to use API_key instead of session token as we are in platform-v1

--- src/services/api-services/infra/iod-env-config-service.js
+++ src/services/api-services/infra/iod-env-config-service.js
@@ -2,50 +2,63 @@
  * Created by avidan on 25-05-15.
  */
 angular
-  .module('iod-client')
-  .factory('iodEnvConfigService', IodEnvConfigService);
+	.module('iod-client')
+	.factory('iodEnvConfigService', IodEnvConfigService);
 
 IodEnvConfigService.$inject = ['envConfig', '$log'];
 
 /* @ngInject */
 function IodEnvConfigService(envConfig, $log) {
-  $log = $log.getInstance("IodEnvConfigService");
-  var isConfigValid = true;
-  var IOD_HOST_URL;
-  var IOD_PORTAL_URL;
+	//$log = $log.getInstance("IodEnvConfigService");
+	var isConfigValid = true;
+	var IOD_HOST_URL;
+	var IOD_PORTAL_URL;
+	var API_KEY;
+
+	try {
+		IOD_HOST_URL = envConfig.iod_config.protocol + '://' + envConfig.iod_config.domain + ( envConfig.iod_config.port ? ':' + envConfig.iod_config.port : '');
+		IOD_PORTAL_URL = envConfig.portal_config.protocol + '://' + envConfig.portal_config.domain + ( envConfig.portal_config.port ? ':' + envConfig.portal_config.port : '');
+	} catch (e) {
+		IOD_HOST_URL = '';
+		IOD_PORTAL_URL = '';
+		$log.error('Configuration error! one of the IOD configuration attributes is missing');
+		isConfigValid = false;
+	}
 
 
-  try {
-    IOD_HOST_URL = envConfig.iod_config.protocol + '://' + envConfig.iod_config.domain + ( envConfig.iod_config.port ? ':' + envConfig.iod_config.port : '');
-    IOD_PORTAL_URL = envConfig.portal_config.protocol + '://' + envConfig.portal_config.domain + ( envConfig.portal_config.port ? ':' + envConfig.portal_config.port : '');
-  } catch (e) {
-    IOD_HOST_URL = '';
-    IOD_PORTAL_URL = '';
-    $log.error('Configuration error! one of the IOD configuration attributes is missing');
-    isConfigValid = false;
-  }
+	var service = {
+		getIodHost: getIodHost,
+		getIodPortal: getIodPortal,
+		setApiKey : setApiKey,
+		getApiKey:getApiKey,
+		isEnvConfigValid: isEnvConfigValid
+	};
 
+	return service;
 
-  var service = {
-    getIodHost: getIodHost,
-    getIodPortal: getIodPortal,
-    isEnvConfigValid: isEnvConfigValid
-  };
+	////////////////
 
-  return service;
+	function getIodHost() {
+		return IOD_HOST_URL;
+	}
 
-  ////////////////
+	function getIodPortal() {
+		return IOD_PORTAL_URL;
+	}
 
-  function getIodHost() {
-    return IOD_HOST_URL;
-  }
+	function setApiKey(_apiKey){
+		if(_apiKey && angular.isString(_apiKey)){
+			API_KEY = _apiKey;
+		}
+		return API_KEY
+	}
 
-  function getIodPortal() {
-    return IOD_PORTAL_URL;
-  }
+	function getApiKey(){
+		return API_KEY
+	}
 
-  function isEnvConfigValid() {
-    return isConfigValid;
-  }
+	function isEnvConfigValid() {
+		return isConfigValid;
+	}
 
 }
--- src/services/api-services/infra/iod-http-factory.js
+++ src/services/api-services/infra/iod-http-factory.js
@@ -2,283 +2,281 @@
  * Created by avidan on 28-04-15.
  */
 angular
-  .module('iod-client')
-  .factory('iodHttpService', IodHttpService);
+	.module('iod-client')
+	.factory('iodHttpService', IodHttpService);
 
 IodHttpService.$inject = ['$http', '$log', 'iodEnvConfigService', 'iodSessionToken'];
 
 /* @ngInject */
-function IodHttpService($http, $log, iodEnvConfigService, iodSessionToken) {
-  $log = $log.getInstance("IodHttpService");
-  var that = this;
-  that.sessionToken = null;
+function IodHttpService($http, $log, iodEnvConfigService) {
+	$log = $log.getInstance("IodHttpService");
+	var that = this;
+	that.sessionToken = null;
 
-  var API_LEVEL = 2; // IOD API level, updated to level 2 to support SSO communication
-  var URL_PREFIX = {API: 'api', INFO: 'info', DISCOVERY: 'discovery'};
-  var SYNC = 'sync';
-  var ASYNC = 'async';
-
-
-  var service = {
-    doApiGet: apiGet,
-    doApiPost: apiPost,
-    doApiPostWithoutDataValidation: doApiPostWithoutDataValidation,
-    doApiPut: apiPut,
-    doApiDelete: apiDelete,
-
-    doInfoGet: doInfoGet,
-
-    doDiscoveryGet: doDiscoveryGet,
-
-    doGet: doGet,
-    doPost: doPost,
-    doPut: doPut,
-    doDelete: doDelete
-  };
+	var API_LEVEL = 1;
+	var URL_PREFIX = {API: 'api', INFO: 'info', DISCOVERY: 'discovery'};
+	var SYNC = 'sync';
+	var ASYNC = 'async';
 
 
-  return service;
-
-  ////////////////
-
-  function appendSessionToken(queryParams) {
-    queryParams.append({token: iodSessionToken.getSessionToken()});
-  }
-
-  function doGet(url, urlParams) {
-    var concatUrl, queryParams;
-    var urlPrefix = buildUrlPrefix();
-    concatUrl = urlPrefix + url;
-
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-
-    $log.debug('performing a GET request to ' + concatUrl);
-    return $http({
-      method: 'GET',
-      url: concatUrl,
-      params: queryParams.params
-    });
-  }
-
-  function doPost(url, data, urlParams) {
-    var concatUrl, queryParams, bodyParams;
-    var urlPrefix = buildUrlPrefix();
-    concatUrl = urlPrefix + url;
-
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-
-    if (data instanceof  ReqBodyData) {
-      bodyParams = data;
-    } else {
-      bodyParams = new ReqBodyData(data);
-    }
-
-    $log.debug('performing a POST request to ' + concatUrl);
-    return $http({
-      method: 'POST',
-      url: concatUrl,
-      params: queryParams.params,
-      data: $.param(bodyParams.getSerializeParamaters()),
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    });
-  }
-
-  function doPut(url, urlParams, data) {
-    var concatUrl, queryParams, bodyParams;
-    var urlPrefix = buildUrlPrefix();
-    concatUrl = urlPrefix + url;
-
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-
-    if (data instanceof  ReqBodyData) {
-      bodyParams = data;
-    } else {
-      bodyParams = new ReqBodyData(data);
-    }
-
-    $log.debug('preforming a PUT request to ' + concatUrl);
-    return $http({
-      method: 'PUT',
-      url: concatUrl,
-      params: queryParams.params,
-      data: $.param(bodyParams.getSerializeParamaters()),
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    });
-  }
-
-  function doDelete(url, urlParams, data) {
-    var concatUrl, queryParams, bodyParams;
-
-    var urlPrefix = buildUrlPrefix();
-    concatUrl = urlPrefix + url;
-
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-
-    if (data instanceof  ReqBodyData) {
-      bodyParams = data;
-    } else {
-      bodyParams = new ReqBodyData(data);
-    }
-
-    $log.debug('preforming a DELETE request to ' + concatUrl);
-    return $http({
-      method: 'DELETE',
-      url: concatUrl,
-      params: queryParams.params,
-      data: $.param(bodyParams.getSerializeParamaters()),
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-    });
-  }
-
-  function doInfoGet(url, urlParams) {
-    var concatUrl = buildInfoPrefix(url);
-    var queryParams;
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-    appendSessionToken(queryParams);
-
-    $log.debug('calling INFO get to ' + concatUrl);
-    return doGet(concatUrl, queryParams)
-  }
-
-  function doDiscoveryGet(url, urlParams) {
-    var concatUrl = buildDiscoveryPrefix(url);
-    var queryParams;
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-    appendSessionToken(queryParams);
-
-    $log.debug('calling DISCOVERY get to ' + concatUrl);
-    return doGet(concatUrl, queryParams)
-  }
+	var service = {
+		doApiGet: apiGet,
+		doApiPost: apiPost,
+		doApiPostWithoutDataValidation: doApiPostWithoutDataValidation,
+		doApiPut: apiPut,
+		doApiDelete: apiDelete,
+		doInfoGet: doInfoGet,
+		doDiscoveryGet: doDiscoveryGet
+	};
 
 
-  function apiGet(url, urlParams, headerParams, isAsync) {
-    var urlPrefix = buildApiPrefix(url, isAsync);
-    var queryParams;
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-    appendSessionToken(queryParams);
+	return service;
 
-    //concatUrl = urlPrefix + queryParams.toReqStr();
-    $log.debug('calling API get to ' + urlPrefix + ', with :' + headerParams);
-    return doGet(urlPrefix, queryParams);
-  }
+	////////////////
 
-  function apiPost(url, data, urlParams, isAsync) {
-    var queryParams;
-    var urlPrefix = buildApiPrefix(url, isAsync);
-    //if (urlParams instanceof  ReqQueryParams) {
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-    appendSessionToken(queryParams);
-    $log.debug('calling API POST to ' + urlPrefix);
-    return doPost(urlPrefix, data, queryParams)
-  }
 
-  /**
-   * Submit a HTTP POST request to api.idolondemand, the call is done without any validation on the request body data to allow submitting calls without the factory HTTP configurations.
-   * @param url the URL suffix to attach to the api.idolondemand prefix
-   * @param data - request body data,expecting a JSON object.
-   * @param urlParams - request query params, expecting a ReqQueryParams object or JSON object.
-   * @param reqConfigObj - request additional configurations, please see AnguleJS $http documentation.
-   * @param isAsync - is request should be done in a Async operation.
-   * @returns the $http response promise.
-   */
-  function doApiPostWithoutDataValidation(url, data, urlParams, reqConfigObj, isAsync) {
-    var queryParams, concatUrl;
-    data = data || {};
-    reqConfigObj = reqConfigObj || {};
+	function doGet(url, urlParams) {
+		var concatUrl, queryParams;
+		var urlPrefix = buildUrlPrefix();
+		concatUrl = urlPrefix + url;
 
-    if (urlParams instanceof ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-    appendSessionToken(queryParams);
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
 
-    concatUrl = [buildUrlPrefix(), buildApiPrefix(url, isAsync)].join('');
+		$log.debug('performing a GET request to ' + concatUrl);
+		return $http({
+			method: 'GET',
+			url: concatUrl,
+			params: queryParams.params
+		});
+	}
 
-    $log.debug('performing a POST request to ' + concatUrl);
+	function doPost(url, data, urlParams) {
+		var concatUrl, queryParams, bodyParams;
+		var urlPrefix = buildUrlPrefix();
+		concatUrl = urlPrefix + url;
 
-    var reqConfig = _.extend({
-      method: 'POST',
-      url: concatUrl,
-      params: queryParams.params,
-      data: data
-    }, reqConfigObj);
-    return $http(reqConfig);
-  }
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
 
-  function apiPut(url, data, urlParams, isAsync) {
-    var queryParams;
-    var urlPrefix = buildApiPrefix(url, isAsync);
-    if (urlParams instanceof  ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-    appendSessionToken(queryParams);
-    $log.debug('calling API PUT to ' + urlPrefix);
-    return doPut(urlPrefix, queryParams, data)
-  }
+		if (data instanceof  ReqBodyData) {
+			bodyParams = data;
+		} else {
+			bodyParams = new ReqBodyData(data);
+		}
 
-  function apiDelete(url, urlParams, data, isAsync) {
-    var queryParams;
-    var urlPrefix = buildApiPrefix(url, isAsync);
-    if (urlParams instanceof  ReqQueryParams) {
-      queryParams = urlParams;
-    } else {
-      queryParams = new ReqQueryParams(urlParams);
-    }
-    appendSessionToken(queryParams);
-    $log.debug('calling API PUT to ' + urlPrefix);
-    return doDelete(urlPrefix, queryParams, data)
-  }
+		$log.debug('performing a POST request to ' + concatUrl);
+		return $http({
+			method: 'POST',
+			url: concatUrl,
+			params: queryParams.params,
+			data: $.param(bodyParams.getSerializeParamaters()),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		});
+	}
 
-  function buildUrlPrefix() {
-    return iodEnvConfigService.getIodHost() + '/' + API_LEVEL + '/';
-  }
+	function doPut(url, urlParams, data) {
+		var concatUrl, queryParams, bodyParams;
+		var urlPrefix = buildUrlPrefix();
+		concatUrl = urlPrefix + url;
 
-  function buildApiPrefix(url, isAsync) {
-    isAsync = isAsync || false;
-    return [URL_PREFIX.API, isAsync ? ASYNC : SYNC, url].join('/');
-  }
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
 
-  function buildInfoPrefix(url) {
-    return [URL_PREFIX.INFO, url].join('/');
-  }
+		if (data instanceof  ReqBodyData) {
+			bodyParams = data;
+		} else {
+			bodyParams = new ReqBodyData(data);
+		}
 
-  function buildDiscoveryPrefix(url) {
-    return [URL_PREFIX.DISCOVERY, url].join('/');
-  }
+		$log.debug('preforming a PUT request to ' + concatUrl);
+		return $http({
+			method: 'PUT',
+			url: concatUrl,
+			params: queryParams.params,
+			data: $.param(bodyParams.getSerializeParamaters()),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).then(function (d) {
+			return d.data
+		});
+	}
+
+	function doDelete(url, urlParams, data) {
+		var concatUrl, queryParams, bodyParams;
+
+		var urlPrefix = buildUrlPrefix();
+		concatUrl = urlPrefix + url;
+
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
+
+		if (data instanceof  ReqBodyData) {
+			bodyParams = data;
+		} else {
+			bodyParams = new ReqBodyData(data);
+		}
+
+		$log.debug('preforming a DELETE request to ' + concatUrl);
+		return $http({
+			method: 'DELETE',
+			url: concatUrl,
+			params: queryParams.params,
+			data: $.param(bodyParams.getSerializeParamaters()),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		});
+	}
+
+	function doInfoGet(url, urlParams) {
+		var concatUrl = buildInfoPrefix(url);
+		var queryParams;
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
+		appendSessionToken(queryParams);
+
+		$log.debug('calling INFO get to ' + concatUrl);
+		return doGet(concatUrl, queryParams)
+	}
+
+	function doDiscoveryGet(url, urlParams) {
+		var concatUrl = buildDiscoveryPrefix(url);
+		var queryParams;
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
+		appendSessionToken(queryParams);
+
+		$log.debug('calling DISCOVERY get to ' + concatUrl);
+		return doGet(concatUrl, queryParams)
+	}
+
+
+	function apiGet(url, urlParams, headerParams, isAsync) {
+		var urlPrefix = buildApiPrefix(url, isAsync);
+		var queryParams;
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
+		appendSessionToken(queryParams);
+
+		//concatUrl = urlPrefix + queryParams.toReqStr();
+		$log.debug('calling API get to ' + urlPrefix + ', with :' + headerParams);
+		return doGet(urlPrefix, queryParams).then(function (response){return response.data});
+	}
+
+	function apiPost(url, data, urlParams, isAsync) {
+		var queryParams;
+		var urlPrefix = buildApiPrefix(url, isAsync);
+		//if (urlParams instanceof  ReqQueryParams) {
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
+		appendSessionToken(queryParams);
+		$log.debug('calling API POST to ' + urlPrefix);
+		return doPost(urlPrefix, data, queryParams).then(function (response){return response.data});
+	}
+
+	/**
+	 * Submit a HTTP POST request to api.idolondemand, the call is done without any validation on the request body data to allow submitting calls without the factory HTTP configurations.
+	 * @param url the URL suffix to attach to the api.idolondemand prefix
+	 * @param data - request body data,expecting a JSON object.
+	 * @param urlParams - request query params, expecting a ReqQueryParams object or JSON object.
+	 * @param reqConfigObj - request additional configurations, please see AnguleJS $http documentation.
+	 * @param isAsync - is request should be done in a Async operation.
+	 * @returns the $http response promise.
+	 */
+	function doApiPostWithoutDataValidation(url, data, urlParams, reqConfigObj, isAsync) {
+		var queryParams, concatUrl;
+		data = data || {};
+		reqConfigObj = reqConfigObj || {};
+
+		if (urlParams instanceof ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
+		appendSessionToken(queryParams);
+
+		concatUrl = [buildUrlPrefix(), buildApiPrefix(url, isAsync)].join('');
+
+		$log.debug('performing a POST request to ' + concatUrl);
+
+		var reqConfig = _.extend({
+			method: 'POST',
+			url: concatUrl,
+			params: queryParams.params,
+			data: data,
+			transformRequest: angular.identity,
+			headers: {'Content-Type': undefined}
+		}, reqConfigObj);
+		return $http(reqConfig).then(function (response){return response.data});
+	}
+
+	function apiPut(url, data, urlParams, isAsync) {
+		var queryParams;
+		var urlPrefix = buildApiPrefix(url, isAsync);
+		if (urlParams instanceof  ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
+		appendSessionToken(queryParams);
+		$log.debug('calling API PUT to ' + urlPrefix);
+		return doPut(urlPrefix, queryParams, data).then(function (response){return response.data})
+	}
+
+	function apiDelete(url, urlParams, data, isAsync) {
+		var queryParams;
+		var urlPrefix = buildApiPrefix(url, isAsync);
+		if (urlParams instanceof  ReqQueryParams) {
+			queryParams = urlParams;
+		} else {
+			queryParams = new ReqQueryParams(urlParams);
+		}
+		appendSessionToken(queryParams);
+		$log.debug('calling API PUT to ' + urlPrefix);
+		return doDelete(urlPrefix, queryParams, data).then(function (response){return response.data})
+	}
+
+	function appendSessionToken(queryParams) {
+		queryParams.append({apiKey: iodEnvConfigService.getApiKey()});
+	}
+
+	function buildUrlPrefix() {
+		return iodEnvConfigService.getIodHost() + '/' + API_LEVEL + '/';
+	}
+
+	function buildApiPrefix(url, isAsync) {
+		isAsync = isAsync || false;
+		return [URL_PREFIX.API, isAsync ? ASYNC : SYNC, url].join('/');
+	}
+
+	function buildInfoPrefix(url) {
+		return [URL_PREFIX.INFO, url].join('/');
+	}
+
+	function buildDiscoveryPrefix(url) {
+		return [URL_PREFIX.DISCOVERY, url].join('/');
+	}
 
 }
