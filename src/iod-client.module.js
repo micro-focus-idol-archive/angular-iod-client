a1ba6d1ac990ff97d0ba9bcc410d60c925a093e4
Itsik Avidan <itsik.avidan@hp.com>
2016-02-17 16:51:46 +0200

    Removed envConfig module definition

--- src/iod-client.module.js
+++ src/iod-client.module.js
@@ -6,20 +6,3 @@ angular.module('iod-client', [])
     $httpProvider.defaults.withCredentials = true; // Setting to true to allow cookies to pass in order to allow IOD's SSO
   }]);
 
-angular.module('envConfig', []).provider("envConfig", function () {
-  this.$get = function () {
-    var envConfig = {};
-    var q = jQuery.ajax({
-      type: 'GET',
-      url: '/config.json',
-      cache: false,
-      async: false,
-      contentType: 'application/json',
-      dataType: 'json'
-    });
-    if (q.status === 200) {
-      angular.extend(envConfig, angular.fromJson(q.responseText));
-    }
-    return envConfig;
-  };
-});
