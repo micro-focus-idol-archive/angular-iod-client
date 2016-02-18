49e4210ed501b7d4f13774f33e45faa4dea4cf05
Itsik Avidan <itsik.avidan@hp.com>
2016-02-18 17:48:51 +0200

    Aligned iod-connector-service.js to platform v1 and adding service documentation

--- src/services/api-services/categories/iod-connector-service.js
+++ src/services/api-services/categories/iod-connector-service.js
@@ -1,149 +1,241 @@
+'use strict';
 /**
- * Created by avidan on 10-05-15.
+ * @ngdoc service
+ * @name iod-client.iodConnectorService
+ * @description
+ * # iodConnectorService
+ * Wraps API calls for the HOD's Connector APIs.
+ *
+ * For more information about connectors see [Connectors](https://dev.havenondemand.com/docs/Connectors.html)
  */
 angular
-  .module('iod-client')
-  .factory('iodConnectorServiceNew', IodConnectorService);
+	.module('iod-client')
+	.factory('iodConnectorService', IodConnectorService);
 
 IodConnectorService.$inject = ['iodHttpService', '$log'];
 
 /* @ngInject */
 function IodConnectorService(iodHttpService, $log) {
-  $log = $log.getInstance('IodConnectorService');
+	var CONNECTOR_FLAVORS = {
+		WEB: 'web_cloud',
+		FS: 'filesystem_onsite',
+		SHAREPOINT: "remote_sharepoint_onsite",
+		DROPBOX: "dropbox_cloud"
+	};
 
-  var CONNECTOR_FLAVORS = {
-    WEB: 'web_cloud',
-    FS: 'filesystem_onsite',
-    SHAREPOINT: "remote_sharepoint_onsite",
-    DROPBOX: "dropbox_cloud"
-  };
+	var ACTIONS = {
+		ADD_TO_INDEX: "addtotextindex"
+	};
 
-  var ACTIONS = {
-    ADD_TO_INDEX: "addtotextindex"
-  };
+	var service = {
+		createConnector: createConnector,
+		deleteConnector: deleteConnector,
+		updateConnector: updateConnector,
+		startConnector: startConnector,
+		stopConnector: stopConnector,
+		connectorStatus: connectorStatus,
+		retrieveConfig: retrieveConfig,
+		cancelConnectorSchedule: cancelConnectorSchedule,
+		connectorHistory: connectorHistory
+	};
 
-  /* var SUPPORTED_CONNECTOR_TYPES= {
-   WEB: "web"
-   };
-   */
-  var service = {
-    createConnector: createConnector,
-    deleteConnector: deleteConnector,
-    updateConnector: updateConnector,
-    startConnector: startConnector,
-    stopConnector: stopConnector,
-    connectorStatus: connectorStatus,
-    retrieveConfig: retrieveConfig,
-    cancelConnectorSchedule: cancelConnectorSchedule,
-    connectorHistory: connectorHistory
-  };
+	return service;
 
-  return service;
+	////////////////
 
-  ////////////////
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#createConnector
+	 * @methodOf iod-client.iodConnectorService
+	 *
+	 * @param {string} connectorFlavor The flavor of the connector to create
+	 * @param {string} connectorName The name of the connector to create. Maximum length of 100 characters.
+	 * @param {string} url For a Web or SharePoint connector, the
+	 * @param {string} indexName Index name
+	 * @param {json=} config A JSON object defining the connector configuration.
+	 * @param {json=} optionalParams JSON object defining additional optional parameters according to the HOD updateConnector v1 API specifications
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 *
+	 */
+	function createConnector(connectorType, connectorName, url, indexName,advancedConnectorParams, optionalParams) {
+		var connectorResUrl = 'createconnector/v1';
 
-  function createConnector(connectorType, connectorName, url, indexName, interval, units, occurrences, advancedConnectorParams, credentialsParams) {
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/v1';
+		var configObj = advancedConnectorParams !== undefined ? advancedConnectorParams : {};
 
-    var configObj = advancedConnectorParams !== undefined ? advancedConnectorParams : {};
+		switch (connectorType) {
+			case CONNECTOR_FLAVORS.WEB:
+			case CONNECTOR_FLAVORS.SHAREPOINT:
+				configObj.url = url;
+				break;
+			case CONNECTOR_FLAVORS.FS:
+				configObj.directoryPathCSVs = url;
+				break
+		}
 
-    switch (connectorType) {
-      case CONNECTOR_FLAVORS.WEB:
-      case CONNECTOR_FLAVORS.SHAREPOINT:
-        configObj.url = url;
-        break;
-      case CONNECTOR_FLAVORS.FS:
-        configObj.directoryPathCSVs = url;
-        break
-    }
+		var config = configObj;
+		var dest = {"action": ACTIONS.ADD_TO_INDEX, "index": indexName};
 
-    var config = configObj;
-    var dest = {"action": ACTIONS.ADD_TO_INDEX, "index": indexName};
+		var params = new ReqBodyData();
+		params.append({'connector': connectorName})
+		params.append({"config": config});
+		params.append({"flavor": connectorType});
+		params.append({"destination": dest});
+		if (optionalParams) {
+			params.append(params.append({"destination": dest}));
+		}
+		return iodHttpService.doApiPost(connectorResUrl, params);
+	}
 
-    var params = new ReqBodyData();
-    params.append({"config": config});
-    params.append({"flavor": connectorType});
-    params.append({"destination": dest});
-    if (interval) {
-      var scheduleVal = {"frequency": {"frequency_type": units, "interval": interval}};
-      if (occurrences !== undefined && occurrences != "" && occurrences > 0)
-        scheduleVal.occurrences = occurrences;
-      //var schedule = JSON.stringify(scheduleVal);
-      var schedule = scheduleVal;
-      params.append({"schedule": schedule});
-    }
-    if (credentialsParams && credentialsParams.credentials) {
-      params.append({'credentials': credentialsParams.credentials});
-      params.append({'credentials_license': credentialsParams.credentials_license});
-      params.append({'credentials_policy': credentialsParams.credentials_policy});
-    }
-    return iodHttpService.doApiPost(connectorResUrl, params);
-  }
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#deleteConnector
+	 * @methodOf iod-client.iodConnectorService
+	 *
+	 * @param {string} connectorName The name of the connector to create. Maximum length of 100 characters.
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 */
+	function deleteConnector(connectorName) {
+		$log.debug("deleting connector {0}", [connectorName]);
+		var connectorResUrl = 'deleteconnector/v1';
+		var reqParams = new ReqQueryParams({connector: connectorName})
+		return iodHttpService.doApiGet(connectorResUrl, reqParams);
+	}
 
-  function deleteConnector(connectorName) {
-    $log.debug("deleting connector {0}", [connectorName]);
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/v1';
-    return iodHttpService.doApiDelete(connectorResUrl);
-  }
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#retrieveConfig
+	 * @methodOf iod-client.iodConnectorService
+	 *
+	 * @param {string} connectorName The name of the connector to create. Maximum length of 100 characters.
+	 * @param {json=} optionalParams JSON object defining additional optional parameters according to the HOD updateConnector v1 API specifications
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 */
+	function retrieveConfig(connectorName, optionalParams) {
+		$log.debug("retrieveConfig {0}", [connectorName]);
 
-  function retrieveConfig(connectorName) {
-    $log.debug("retrieveConfig {0}", [connectorName]);
+		var params = new ReqQueryParams();
+		params.append({"connector": connectorName});
+		params.append({"output": "config_attributes"});
+		params.append(optionalParams)
+		var connectorResUrl = 'retrieveconfig/v1';
+		return iodHttpService.doApiGet(connectorResUrl, params);
+	}
 
-    var params = new ReqQueryParams();
-    params.append({"output": "config_attributes"});
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/config/v1';
-    return iodHttpService.doApiGet(connectorResUrl, params);
-  }
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#connectorStatus
+	 * @methodOf iod-client.iodConnectorService
+	 *
+	 * @param {string} connectorName The name of the connector to create. Maximum length of 100 characters.
+	 * @param {json=} optionalParams JSON object defining additional optional parameters according to the HOD updateConnector v1 API specifications
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 */
+	function connectorStatus(connectorName,optionalParams) {
+		$log.debug("connectorStatus {0}", [connectorName]);
 
-  function connectorStatus(connectorName) {
-    $log.debug("connectorStatus {0}", [connectorName]);
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/status/v1';
-    return iodHttpService.doApiGet(connectorResUrl);
-  }
+		var connectorResUrl = 'connectorstatus/v1';
+		var params = new ReqQueryParams();
+		params.append({"connector": connectorName});
+		params.append(optionalParams)
+		return iodHttpService.doApiGet(connectorResUrl, params);
+	}
 
-  function updateConnector(connectorName, configObj, indexName, scheduleObj) {
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/v1';
 
-    var bodyParams = new ReqBodyData();
-    if (configObj !== null && configObj !== undefined) {
-      bodyParams.append({config: configObj});
-    }
-    if (scheduleObj.enabled) {
-      var schedule = {"frequency": {"frequency_type": scheduleObj.units, "interval": scheduleObj.interval}}
-      if (scheduleObj.limitOccurrences)
-        schedule.occurrences = scheduleObj.occurrences;
-      bodyParams.append({schedule: schedule});
-    }
-    if (indexName != undefined) {
-      var dest = {"action": ACTIONS.ADD_TO_INDEX, "index": indexName};
-      bodyParams.append({destination: dest});
-    }
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#updateConnector
+	 * @methodOf iod-client.iodConnectorService
+	 * @description Update existing connector configurations
+	 *
+	 *
+	 * @param {string} connectorName The name of the connector
+	 * @param {json=} configObj A JSON object defining the connector configuration.
+	 * @param {string=} indexName the Index name the connector send data to
+	 * @param {json=} optionalParams JSON object defining additional optional parameters according to the HOD updateConnector v1 API specifications
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 */
+	function updateConnector(connectorName, configObj, indexName, optionalParams) {
+		var connectorResUrl = 'updateconnector/v1';
+		var bodyParams = new ReqBodyData();
+		bodyParams.append({connector:connectorName})
+		if (configObj !== null && configObj !== undefined) {
+			bodyParams.append({config: configObj});
+		}
+		if (indexName != undefined) {
+			var dest = {action: ACTIONS.ADD_TO_INDEX, "index": indexName};
+			bodyParams.append({destination: dest});
+		}
+		if(optionalParams){
+			bodyParams.append(optionalParams)
+		}
+		return iodHttpService.doApiPost(connectorResUrl, bodyParams);
+	}
 
-    return iodHttpService.doApiPut(connectorResUrl, bodyParams);
-  }
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#startConnector
+	 * @methodOf iod-client.iodConnectorService
+	 * @description start existing connector
 
-  function startConnector(connectorName, indexChanged) {
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/start/v1';
-    var params = new ReqBodyData();
-    if (indexChanged) {
-      params.append({ignore_previous_state: true});
-    }
-    return iodHttpService.doApiPost(connectorResUrl, params);
-  }
+	 * @param {string} connectorName The name of the connector
+	 * @param {json=} optionalParams JSON object defining additional optional parameters according to the HOD updateConnector v1 API specifications
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 */
+	function startConnector(connectorName, optionalParams) {
+		var connectorResUrl = '/startconnector/v1';
+		var params = new ReqBodyData();
+		params.append({connector:connectorName})
+		if (optionalParams) {
+			params.append(optionalParams);
+		}
+		return iodHttpService.doApiPost(connectorResUrl, params);
+	}
 
-  function stopConnector(connectorName) {
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/stop/v1';
-    return iodHttpService.doApiPost(connectorResUrl);
-  }
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#stopConnector
+	 * @methodOf iod-client.iodConnectorService
+	 * @description stops a connector schedule.
+	 * @param {string} connectorName The name of the connector
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 */
+	function stopConnector(connectorName) {
+		var connectorResUrl = '/stopconnector/v1';
+		var params = new ReqBodyData();
+		params.append({connector:connectorName})
+		return iodHttpService.doApiPost(connectorResUrl,params);
+	}
 
-  function cancelConnectorSchedule(connectorName) {
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/cancelschedule/v1';
-    return iodHttpService.doApiPost(connectorResUrl, params);
-  }
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#cancelConnectorSchedule
+	 * @methodOf iod-client.iodConnectorService
+	 * @description Cancels a connector schedule.
+	 * @param {string} connectorName The name of the connector
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 */
+	function cancelConnectorSchedule(connectorName) {
+		var connectorResUrl = 'cancelconnectorschedule/v1';
+		var params = new ReqBodyData();
+		params.append({connector:connectorName})
+		return iodHttpService.doApiPost(connectorResUrl,params);
+	}
 
-  function connectorHistory(connectorName) {
-    var connectorResUrl = 'connector/' + encodeURIComponent(connectorName) + '/history/v1';
-    return iodHttpService.doApiGet(connectorResUrl, params);
-  }
+	/**
+	 * @ngdoc
+	 * @name iod-client.iodConnectorService#connectorHistory
+	 * @methodOf iod-client.iodConnectorService
+	 * @description Returns connector status history information.
+	 * @param {json=} optionalParams JSON object defining additional optional parameters according to the HOD updateConnector v1 API specifications
+	 * @returns {httpPromise} resolve with fetched data, or fails with error description.
+	 */
+	function connectorHistory(optionalParams) {
+		var connectorResUrl = 'connectorhistory/v1';
+		var params = new ReqQueryParams();
+		if(optionalParams){
+			params.append(optionalParams)
+		}
+		return iodHttpService.doApiGet(connectorResUrl,params);
+	}
 
 }
--- test/spec/services/api-services/categories/iod-connector-service.test.js
+++ test/spec/services/api-services/categories/iod-connector-service.test.js
@@ -1,122 +1,128 @@
 /**
  * Created by avidan on 13-05-15.
  */
-describe('IOD-Connector-Service Test', function (){
+describe('IOD-Connector-Service Test', function () {
 
-    var scope,
-        rootScope,
-        $q,
-        iodHttpServiceMock,
-        logMock,
-        serviceUnderTest;
+	var scope,
+		rootScope,
+		$q,
+		iodHttpServiceMock,
+		logMock,
+		serviceUnderTest;
 
-    beforeEach(angular.mock.module("iod-client"));
+	beforeEach(angular.mock.module("iod-client"));
 
-    beforeEach(module("iod-client", function ($provide) {
-        iodHttpServiceMock = jasmine.createSpyObj('iodHttpService', ['']);
-        $provide.value('iodHttpService', iodHttpServiceMock);
-        logMock = jasmine.createSpyObj('logMock', ['']);
-        logMock.getInstance = jasmine.createSpy('getInstance').and.returnValue({debug:function(){},error:function (){}});
-        $provide.value('$log', logMock);
-    }));
+	beforeEach(module("iod-client", function ($provide) {
+		iodHttpServiceMock = jasmine.createSpyObj('iodHttpService', ['']);
+		$provide.value('iodHttpService', iodHttpServiceMock);
+		logMock = jasmine.createSpyObj('logMock', ['debug','error']);
+		$provide.value('$log', logMock);
+	}));
 
+	beforeEach(
+		inject(function ($rootScope, _$q_, iodConnectorService) {
+			rootScope = $rootScope;
+			scope = rootScope.$new();
+			serviceUnderTest = iodConnectorService;
+			$q = _$q_;
+		}));
 
-    beforeEach(
-        inject(function ($rootScope, _$q_, iodConnectorServiceNew) {
-            rootScope = $rootScope;
-            scope = rootScope.$new();
-            serviceUnderTest = iodConnectorServiceNew;
-            $q = _$q_;
-        }));
+	beforeEach(function () {
+		iodHttpServiceMock.doApiGet = createIODConnectorMockSpyFunction($q, "doApiGet", {});
+		iodHttpServiceMock.doApiPost = createIODConnectorMockSpyFunction($q, "doApiPost", {});
+	});
 
-    beforeEach(function () {
-        iodHttpServiceMock.doApiGet = createIODConnectorMockSpyFunction($q, "doApiGet", {});
-        iodHttpServiceMock.doApiPost = createIODConnectorMockSpyFunction($q, "doApiPost", {});
-    });
+	describe('validate the service init', function () {
+		it('service\'s methods should be defined', function () {
+			expect(serviceUnderTest.createConnector).toBeDefined();
+			expect(serviceUnderTest.deleteConnector).toBeDefined();
+			expect(serviceUnderTest.updateConnector).toBeDefined();
+			expect(serviceUnderTest.startConnector).toBeDefined();
+			expect(serviceUnderTest.stopConnector).toBeDefined();
+			expect(serviceUnderTest.connectorStatus).toBeDefined();
+			expect(serviceUnderTest.retrieveConfig).toBeDefined();
+			expect(serviceUnderTest.cancelConnectorSchedule).toBeDefined();
+			expect(serviceUnderTest.connectorHistory).toBeDefined();
+		});
+	});
 
-    describe('validate the service init', function (){
-        it('service\'s methods should be defined', function () {
-            expect(serviceUnderTest.createConnector).toBeDefined();
-            expect(serviceUnderTest.deleteConnector).toBeDefined();
-            expect(serviceUnderTest.updateConnector).toBeDefined();
-            expect(serviceUnderTest.startConnector).toBeDefined();
-            expect(serviceUnderTest.stopConnector).toBeDefined();
-            expect(serviceUnderTest.connectorStatus).toBeDefined();
-            expect(serviceUnderTest.retrieveConfig).toBeDefined();
-            expect(serviceUnderTest.cancelConnectorSchedule).toBeDefined();
-            expect(serviceUnderTest.connectorHistory).toBeDefined();
-            expect(logMock.getInstance).toHaveBeenCalled();
-        });
-    });
+	var ACTIONS = {
+		ADD_TO_INDEX: "addtotextindex"
+	};
 
-    var ACTIONS= {
-        ADD_TO_INDEX: "addtotextindex"
-    };
+	var SUPPORTED_CONNECTOR_TYPES = {
+		WEB: "web"
+	};
 
-    var SUPPORTED_CONNECTOR_TYPES= {
-        WEB: "web"
-    };
-
-    var CONNECTOR_FLAVORS = {
-        WEB:'web_cloud',
-        FS:'filesystem_onsite'
-    };
+	var CONNECTOR_FLAVORS = {
+		WEB: 'web_cloud',
+		FS: 'filesystem_onsite'
+	};
 
 
-    function buildConnectorUrl(connectorName){
-        return ['connector',connectorName,'v1'].join('/')
-    }
+	function buildConnectorUrl(connectorName) {
+		return ['connector', connectorName, 'v1'].join('/')
+	}
 
-    describe('validate the createConnector method', function (){
+	describe('validate the createConnector method', function () {
 
-        describe('for web connector', function (){
-            it('Should call with no expected errors ', function (){
-                var connectorType = 'web_cloud';
-                var connectorName = 'koo';
-                var url = 'http://www.koo.com';
-                var indexName = 'foo';
-                serviceUnderTest.createConnector(connectorType, connectorName, url, indexName);
+		describe('for web connector', function () {
+			it('Should call with no expected errors ', function () {
+				var connectorType = 'web_cloud';
+				var connectorName = 'koo';
+				var url = 'http://www.koo.com';
+				var indexName = 'foo';
+				serviceUnderTest.createConnector(connectorType, connectorName, url, indexName);
+				var wantedConfig = new ReqBodyData({
+					connector:connectorName,
+					flavor: connectorType,
+					config: {
+						url: url
+					},
+					destination: {
+						action: ACTIONS.ADD_TO_INDEX,
+						index: indexName
+					}
+				});
+				expect(iodHttpServiceMock.doApiPost).toHaveBeenCalledWith('createconnector/v1', wantedConfig);
+			});
+		});
 
-                var wantedConfig = new ReqBodyData({
-//                    type:SUPPORTED_CONNECTOR_TYPES.WEB,
-                    flavor:connectorType,
-                    config:{
-                        url:url
-                    },
-                    destination:{
-                        action:ACTIONS.ADD_TO_INDEX,
-                        index:indexName
-                    }
-                });
-                expect(iodHttpServiceMock.doApiPost).toHaveBeenCalledWith(buildConnectorUrl(connectorName),wantedConfig);
-            });
-        });
+		describe('for FS connector', function () {
+			it('Should call with no expected errors ', function () {
+				var connectorType = 'filesystem_onsite';
+				var connectorName = 'koo';
+				var url = 'c:\\aaaa\\bb';
+				var indexName = 'foo';
+				serviceUnderTest.createConnector(connectorType, connectorName, url, indexName);
 
-        describe('for FS connector', function (){
-            it('Should call with no expected errors ', function (){
-                var connectorType = 'filesystem_onsite';
-                var connectorName = 'koo';
-                var url = 'c:\\aaaa\\bb';
-                var indexName = 'foo';
-                serviceUnderTest.createConnector(connectorType, connectorName, url, indexName);
-
-                var wantedConfig = new ReqBodyData({
-             //       type:SUPPORTED_CONNECTOR_TYPES.WEB,
-                    flavor:connectorType,
-                    config:{
-                        directoryPathCSVs :url
-                    },
-                    destination:{
-                        action:ACTIONS.ADD_TO_INDEX,
-                        index:indexName
-                    }
-                });
-                expect(iodHttpServiceMock.doApiPost).toHaveBeenCalledWith(buildConnectorUrl(connectorName),wantedConfig);
-            })
-        })
-
+				var wantedConfig = new ReqBodyData({
+					connector:connectorName,
+					flavor: connectorType,
+					config: {
+						directoryPathCSVs: url
+					},
+					destination: {
+						action: ACTIONS.ADD_TO_INDEX,
+						index: indexName
+					}
+				});
+				expect(iodHttpServiceMock.doApiPost).toHaveBeenCalledWith('createconnector/v1', wantedConfig);
+			})
+		})
 
 
-    })
+	})
+
+	describe('validate the delete connector API', function (){
+		it('should call with valid connector name', function (){
+			var connectorName = 'foo';
+			var wantedReqQueryParams = new ReqQueryParams({
+				connector:connectorName
+			})
+			serviceUnderTest.deleteConnector(connectorName)
+			expect(iodHttpServiceMock.doApiGet).toHaveBeenCalledWith('deleteconnector/v1', wantedReqQueryParams)
+		})
+	})
 
 });
