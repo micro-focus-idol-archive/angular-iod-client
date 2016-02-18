83074a8f856ccb73fdd20b8f110bd8d9f17437d4
Itsik Avidan <itsik.avidan@hp.com>
2016-02-18 18:40:53 +0200

    Adding fist doc draft

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,43 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "iod-client",
+      "shortName": "iod-client",
+      "type": "overview",
+      "moduleName": "iod-client",
+      "shortDescription": "This module is a utility client to wrap HavenOnDemand APIs as an Angular.js client",
+      "keywords": "additional angular api apis client documentation havenondemand iod-client js module overview utility wrap"
+    },
+    {
+      "section": "api",
+      "id": "iod-client.iodConnectorService",
+      "shortName": "iod-client.iodConnectorService",
+      "type": "service",
+      "moduleName": "iod-client",
+      "shortDescription": "iodConnectorService",
+      "keywords": "additional api apis calls cancelconnectorschedule cancels characters config configobj configuration configurations connector connectorflavor connectorhistory connectorname connectors connectorstatus create createconnector data defining deleteconnector description error existing fails fetched flavor havenondemand history hod html indexname iod-client iodconnectorservice json length maximum object optional optionalparams parameters resolve retrieveconfig returns schedule send service sharepoint specifications start startconnector status stopconnector stops update updateconnector url v1 web wraps"
+    },
+    {
+      "section": "api",
+      "id": "iod-client.iodHttpService",
+      "shortName": "iod-client.iodHttpService",
+      "type": "service",
+      "moduleName": "iod-client",
+      "shortDescription": "iodHttpService",
+      "keywords": "$http $httpprovider additional allow allows angulejs api api-key apidelete apiget apipost apiput appended applied async asynchronous asynchronously attach attached body call calls check codes complete configurations connection create data default delete description discovery documentation dodiscoveryget doinfoget error expect expecting fails false fetched form functions haven havenondemand header headerparams hod htm html http https ideal idolondemand info iod-client iod-env-config-service iodhttpservice isasync job json lost message method object ondemand operation params post prefix prior processes promise query quick receives relative reqconfigobj reqqueryparams request requests resolve response resubmit returns service set setapikey single small status submit submitting suffix sync synchronous synchronously tenant track url urlparams validation wanted wraps"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
