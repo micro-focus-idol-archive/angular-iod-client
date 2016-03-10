3feaef3b676a444b3e67222a9430cc6dbf793b9a
Itsik Avidan <itsik.avidan@hp.com>
2016-03-10 16:24:50 +0200

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,43 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "hod-client.hodHttpService",
+      "shortName": "hod-client.hodHttpService",
+      "type": "service",
+      "moduleName": "hod-client",
+      "shortDescription": "hodHttpService",
+      "keywords": "$http $httpprovider additional allow allows angulejs api api-key apidelete apiget apipost apiput appended applied async asynchronous asynchronously attach attached body call calls check codes complete configurations connection create data default delete description discovery documentation dodiscoveryget doinfoget error expect expecting fails false fetched form functions haven havenondemand header headerparams hod hod-client hodhttpservice htm html http https ideal idolondemand info iod-env-config-service isasync job json lost message method object ondemand operation params post prefix prior processes promise query quick receives relative reqconfigobj reqqueryparams request requests resolve response resubmit returns service set setapikey single small status submit submitting suffix sync synchronous synchronously tenant track url urlparams validation wanted wraps"
+    },
+    {
+      "section": "api",
+      "id": "hod-client.iodConnectorService",
+      "shortName": "hod-client.iodConnectorService",
+      "type": "service",
+      "moduleName": "hod-client",
+      "shortDescription": "iodConnectorService",
+      "keywords": "additional api apis calls cancelconnectorschedule cancels characters config configobj configuration configurations connector connectorflavor connectorhistory connectorname connectors connectorstatus create createconnector data defining deleteconnector description error existing fails fetched flavor havenondemand history hod hod-client html indexname iodconnectorservice json length maximum object optional optionalparams parameters resolve retrieveconfig returns schedule send service sharepoint specifications start startconnector status stopconnector stops update updateconnector url v1 web wraps"
+    },
+    {
+      "section": "api",
+      "id": "iod-client",
+      "shortName": "iod-client",
+      "type": "overview",
+      "moduleName": "iod-client",
+      "shortDescription": "This module is a utility client to wrap HavenOnDemand APIs as an Angular.js client",
+      "keywords": "additional angular api apis client documentation havenondemand iod-client js module overview utility wrap"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
